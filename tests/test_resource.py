e58fd7f47037f188a2b1fc8b8320c0ff3c75da29
Cody M Sommer <BassMastaCod@gmail.com>
2025-07-09 15:57:24 -0400

    Refactor search schema logic in Resource class
    Replaces static `_search_schema` attribute with a dynamic `get_search_schema` method. Simplifies schema creation using Pydantic's `create_model` and ensures searchable fields are dynamically resolved. Removes outdated setter and getter methods for `_search_schema`.

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,7 +1,9 @@
-from typing import Any
+from datetime import datetime
+from typing import Any, Optional
 
 import pytest
 from daomodel import DAOModel
+from daomodel.fields import Unsearchable, Identifier, ReferenceTo
 from sqlmodel import SQLModel
 
 from fast_controller import Resource, Controller
@@ -34,6 +36,47 @@ def test_get_path():
     class C(Resource):
         pass
     assert C.get_resource_path() == "/api/c"
+
+
+class Author(Resource, table=True):
+    name: Identifier[str]
+    bio: Optional[str]
+    active: bool = True
+
+
+class Book(Resource, table=True):
+    title: Identifier[str]
+    author_name: Author
+    description: Unsearchable[Optional[str]]
+    page_count: Optional[int]
+    publication_date: Optional[datetime]
+    publisher_name: str = ReferenceTo('publisher.name')
+
+
+class Publisher(Resource, table=True):
+    name: Identifier[str]
+
+    class Meta:
+        searchable_relations = {
+            Book.title, Book.page_count, Book.publication_date,
+            (Book, Author.name),
+            (Book, Author.active)
+        }
+
+
+def test_get_search_schema():
+    actual = Publisher.get_search_schema()
+    assert actual.__name__ == 'PublisherSearchSchema'
+    class Expected(SQLModel):
+        name: str
+        book_title: str
+        book_page_count: int
+        book_publication_date: datetime
+        author_name: str
+        author_active: bool
+    actual_fields = {k: v.annotation for k, v in actual.model_fields.items()}
+    expected_fields = {k: v.annotation for k, v in Expected.model_fields.items()}
+    assert actual_fields == expected_fields
 
 
 # TODO - Not convinced on this design for schema definitions
